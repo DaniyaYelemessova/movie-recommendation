c02a99623682fd7ac21750213f5508f07709af27
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-01 10:23:24 -0400

    Add branching

--- script.js
+++ script.js
@@ -2,25 +2,34 @@ window.addEventListener("load", function() {
   let button = document.querySelector("button")
   button.addEventListener("click", chosenMovie);
 
+  function addClass(){
+    document.querySelector("#comedies").setAttribute("class", "hidden");
+    document.querySelector("#horror").setAttribute("class", "hidden");
+    document.querySelector("#action").setAttribute("class", "hidden");
+    document.querySelector("#drama").setAttribute("class", "hidden");
+    document.querySelector("#crime").setAttribute("class", "hidden");
+  }
+
 
   function chosenMovie(e){
     e.preventDefault();
     // let form = document.querySelector("form");
     let select = document.querySelector("select").value;
+    addClass()
 
     document.querySelector(".movie").innerText = select;
     document.querySelector("#message").removeAttribute("class");
 
     if(select === "Comedy"){
-      document.querySelector("#comedies").removeAttribute("class")
+      document.querySelector("#comedies").removeAttribute("class");
+    }else if(select === "Horror"){
+      document.querySelector("#horror").removeAttribute("class");
+    }else if(select === "Action"){
+      document.querySelector("#action").removeAttribute("class");
+    }else if(select === "Drama"){
+      document.querySelector("#drama").removeAttribute("class");
+    }else if(select === "Crime"){
+      document.querySelector("#crime").removeAttribute("class");
     }
-    
-    
-    
-    
-    
   }
-  
-
-
 })
